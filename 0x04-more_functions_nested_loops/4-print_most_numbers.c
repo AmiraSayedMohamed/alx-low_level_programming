f87623073f54cc5f6e0f034e0ecb0bb420cc3548
AmiraSayedMohamed <amira.sayedza@gamil.com>
2023-08-17 17:39:56 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print the numbers, form 0 to 9
+ *      followed by a new line.
+ *
+ * Return: 0 always (Success)
+ */
+
+void print_most_numbers(void);
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
