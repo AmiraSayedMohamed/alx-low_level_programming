6cd7990724af49eb2ffdd39fb291dca9d13d6d9e
AmiraSayedMohamed <amira.sayedza@gmail.com>
2023-10-03 10:41:26 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,58 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * _strlen - a function to find the length of the string
+ * @s: string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
+}
 
 /**
- * *str_concat - a function concatenate two string
+ * str_concat - concatenate two string
  * @s1: string
  * @s2: string
- * Return: a pointer point to a newly allocated space
+ * Return: pointer
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int size1, size2, i, j;
+	int size1, size2, i;
+	char *n;
 
-	if (s1 == '\0')
-		s1 = NULL;
-	if (s2 == '\0')
-		s2 = NULL;
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 
-	char *n = (chari *)malloc((size1 + size2) * sizeof(char) + 1);
+	n = (char *) malloc((size1 + size2) * sizeof(char) + 1);
 
 	if (n == 0)
 		return (0);
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(m + i) = *(s1 + i);
+		*(n + i) = *(s1 + i);
 
 	for (j = 0; *(s2 + j) != '\0'; j++)
 	{
-		*(m + i) = *(s2 + j);
+		*(n + i) = *(s2 + j);
 		i++;
 	}
-	return (m);
-}
-
-/**
- * _strlen - function to get length
- * @s: string
- * Return: pointer points to the allocated memory space
- */
-
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-
+	return (n);
 }
