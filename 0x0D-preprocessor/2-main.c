5ad08bb78234578bc2114819e128ff04f949394e
AmiraSayedMohamed <amira.sayedza@gmail.com>
2023-10-10 11:02:42 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdion.h>
+
+/**
+ * main - a finction that prints the name of the file program was compiles from
+ * Return: Alwalyes 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
